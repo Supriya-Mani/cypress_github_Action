14115b5e17998918a2bfd4dab8ed1ea03db21082
Supriya-Mani <155082731+Supriya-Mani@users.noreply.github.com>
2024-01-09 17:50:57 +0530

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,46 +1,32 @@
-const merge = require('mochawesome-merge');
-const generateReport = require('mochawesome-report-generator');
 
-module.exports = (on, config) => {
-  on('after:run', (results) => {
-    const reportDir = 'cypress/reports/json'; // Update the path to match your actual JSON report directory
+const { defineConfig } = require("cypress");
 
-    // Merge JSON reports
-    return merge({ files: [`${reportDir}/*.json`] })
-      .then((report) => generateReport(report, { reportDir }))
-      .catch((e) => {
-        console.error(e);
-        process.exit(1);
-      });
-  });
-
-  return {
-    projectId: process.env.CYPRESS_PROJECT_ID || "bcmpwq",
-    video: true,
-    viewportWidth: 2000,
-    viewportHeight: 900,
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'Cypress Inline Reporter',
-      embeddedScreenshots: true,
-      inlineAssets: true,
+module.exports = defineConfig({
+  projectId: process.env.CYPRESS_PROJECT_ID || "bcmpwq",
+  video: true,
+  viewportWidth: 2000,
+  viewportHeight: 900,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Inline Reporter',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  execTimeout: 60000,
+  uncaughtExceptionCaptureLevel: "off",
+  videoTimeout: 60000,
+  videosFolder: "cypress/videos",
+  env: {
+    // Set environment variables as needed
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    chromeWebSecurity: false,
-    defaultCommandTimeout: 10000,
-    execTimeout: 60000,
-    uncaughtExceptionCaptureLevel: "off",
-    videoTimeout: 60000,
-    videosFolder: "cypress/videos",
-    env: {
-      // Set environment variables as needed
-    },
-    e2e: {
-      setupNodeEvents(on, config) {
-        // implement node event listeners here
-        require('cypress-mochawesome-reporter/plugin')(on);
-      },
-      specPattern: 'cypress/Integration/Examples/*.js'
-    }
-  };
-};
+    specPattern: 'cypress/Integration/Examples/*.js'
+  }
+});
