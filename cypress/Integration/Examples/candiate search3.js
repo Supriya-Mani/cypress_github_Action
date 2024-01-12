94da5d1498e27c7c80dc42203331ed922f0fdfc6
Supriya-Mani <155082731+Supriya-Mani@users.noreply.github.com>
2024-01-12 22:37:44 +0530

    Update candiate search3.js

--- cypress/Integration/Examples/candiate search3.js
+++ cypress/Integration/Examples/candiate search3.js
@@ -24,5 +24,9 @@ cy.visit("https://web.staging.xpheno.work/login",{failOnStatusCode: false, })
    cy.xpath('/html/body/div[1]/div[4]/div/main/div/div/div[1]/div[1]/form/div[2]/button[1]').click({force:true})
 
 
+
+
+
+   
     })
 })
